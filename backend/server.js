937df1d518f45280799dccd23507dd828b254b47
christxnele <156040195+christxnele@users.noreply.github.com>
2026-04-04 20:32:34 -0700

    trust proxy for https on render

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import googleRoutes from './routes/google.js';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
