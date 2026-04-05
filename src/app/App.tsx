c1cb99f4eca70fd60bcbf746279ee421f3a0949c
christxnele <156040195+christxnele@users.noreply.github.com>
2026-04-04 21:58:58 -0700

    added toneprovider router in app.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { AuthProvider } from './auth';
+import { ToneProvider } from './context/ToneContext';
 
 export default function App() {
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <ToneProvider>
+        <RouterProvider router={router} />
+      </ToneProvider>
     </AuthProvider>
   );
 }
